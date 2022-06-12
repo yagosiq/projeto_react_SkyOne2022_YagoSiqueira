9a93c5edaf9c12ca921676bd29d87a7c0e4fa512
Yago Siqueira <yagosiqti@gmail.com>
2022-06-12 11:52:15 -0300

    Implemetntação 10. Mostrando as jogadas realizado

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -51,6 +51,30 @@ class Board extends React.Component {
       status = 'Next player ' + (this.state.xIsNext ? 'X':'O');
     }
 
+    const moves = history.map((step, move) => {
+        const desc = move ? 'Go to move # ' + move : 'Go to game start';
+        return(
+            <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
+      return (
+        <div className='game'>
+          <div className='game-board'>
+            <Board squares={current.squares}
+                   onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className='game-info'></div>
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      )
+    }
+  }
+
     this.render() {
         return (
             <div>
