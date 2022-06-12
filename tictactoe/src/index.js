26369b20253c5be960e071e4b7c8b0aa583ef460
Yago Siqueira <yagosiqti@gmail.com>
2022-06-12 11:31:25 -0300

    Adicionando boolean e o status para mostrar quem vai jogar no próximo turno

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -15,13 +15,18 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setsatet({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
+
     }
 
     renderSquare(i) {
@@ -31,7 +36,7 @@ class Board extends React.Component {
       }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
